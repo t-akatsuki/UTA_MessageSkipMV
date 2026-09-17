3ed7734ec90c5039760effecbd733a41582990d3
t-akatsuki <t-akatsuki@utakata-no-yume.net>
2026-09-18 04:48:00 +0900

    add: 戦闘ログにおけるメッセージスキップ処理実体部分を記述。

--- src/extends/rpg_windows.js
+++ src/extends/rpg_windows.js
@@ -79,8 +79,47 @@ Window_ScrollText.prototype.scrollSpeed = function() {
 /* ------------------------------------------------------------------------- */
 /*  Window_BattleLog extends */
 /* ------------------------------------------------------------------------- */
+const _Window_BattleLog_prototype_updateWaitCount = Window_BattleLog.prototype.updateWaitCount;
 
-// Window_BattleLog.prototype.messageSpeedで速度を返している
-// Window_BattleLog.prototype.isFastForwardでボタン判定
-// Windowのterminate処理がないのよね…
-// Scene_Battle.prototype.terminateはあるので、ここでリセットした方が良いかも
+/**
+ * @override
+ * @return {boolean}
+ */
+Window_BattleLog.prototype.updateWaitCount = function() {
+    let ret = _Window_BattleLog_prototype_updateWaitCount.call(this);
+    if (this._waitCount <= 0) {
+        return ret;
+    }
+
+    /* メッセージスキップキーを押下している場合はスキップ速度でカウンタを進める */
+    if (MessageSkipManager.isEnabled(MessageSkipTarget.BATTLE_LOG) && MessageSkipManager.isTriggeredSkipButton()) {
+        /* 元の処理で既に引かれた分を考慮 */
+        const updatedWaitCount = this.isFastForward() ? 3 : 1;
+        const decreaseWaitCount = Math.max(MessageSkipManager.getFastForwardRate(MessageSkipTarget.BATTLE_LOG) - updatedWaitCount, 0);
+        this._waitCount -= decreaseWaitCount;
+        if (this._waitCount < 0) {
+            this._waitCount = 0;
+        }
+        ret = true;
+    }
+
+    return ret;
+};
+
+const _Window_BattleLog_prototype_isFastForward = Window_BattleLog.prototype.isFastForward;
+
+/**
+ * @override
+ * @return {boolean}
+ */
+Window_BattleLog.prototype.isFastForward = function() {
+    let ret = _Window_BattleLog_prototype_isFastForward.call(this);
+
+    /* デフォルト機能を無効に設定した場合、デフォルトの早送り判定で必ずfalseを返す */
+    if (MessageSkipManager.isForceDisabledDefaultSkip(MessageSkipTarget.BATTLE_LOG)) {
+        ret = false;
+    }
+
+    return ret;
+};
+
